13fab0f4a82139c3205be40dd05266ce86dea2ed
Shreya Ray <161076300+forgottenflowers@users.noreply.github.com>
2025-03-04 18:35:08 +0100

    Create kill.sql

---
+++ kill.sql
@@ -0,0 +1,23 @@
+--select * from vis_columns where column_name like '%xxxx%'
+
+select  spid,
+        blocked,
+        cpu,
+        physical_io,
+        memusage,
+        status,
+        login_time,
+        last_batch,
+        open_tran,
+        hostname,
+        program_name,
+        nt_username
+from master..sysprocesses
+where nt_username = 'my_name' and open_tran = 1
+order by case when nt_username = 'priorityuser' then 1 else 2 end,
+        open_tran desc,
+        cpu desc
+
+go
+--kill 116 --go -- exec tran_name go
+--'User does not have permission to use the KILL statement.'
